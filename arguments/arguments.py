ecb15c8d7fc910c20964aed2e6e09f6c3a52e354
Erik de Jonge <erik@a8.nl>
2015-03-04 23:35:18 +0100

    arguments Wednesday 04 March 2015 (week:9 day:63), 23:35:11

---
+++ arguments/arguments.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+"""
+arguments
+erik@a8.nl (04-03-15)
+license: GNU-GPL2
+"""
+
+
+def main():
+    """
+    main
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    
