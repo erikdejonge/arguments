542e06df10384eb414cd182986d54034f77129ac
Erik de Jonge <erik@a8.nl>
2015-03-06 10:53:58 +0100

    -

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ license: GNU-GPL2
 """
 from setuptools import setup
 setup(name='arguments',
-      version='4',
+      version='5',
       description='Argument parser based on docopt',
       url='https://github.com/erikdejonge/arguments',
       author='Erik de Jonge',
