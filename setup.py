25c8b3fb6cbb85adc75b7e82235bb4d476cf58b6
Erik de Jonge <erik@a8.nl>
2015-03-06 16:24:26 +0100

    pip Friday 06 March 2015 (week:9 day:65), 16:24:23

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from future import standard_library
 standard_library.install_aliases()
 from setuptools import setup
 setup(name='arguments',
-      version='13',
+      version='15',
       description='Argument parser based on docopt',
       url='https://github.com/erikdejonge/arguments',
       author='Erik de Jonge',
@@ -24,7 +24,7 @@ setup(name='arguments',
       classifiers=[
           "Programming Language :: Python",
           "Programming Language :: Python :: 3",
-          "Development Status :: Beta",
+          "Development Status :: 4 - Beta ",
           "Intended Audience :: Developers",
           "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
           "Operating System :: POSIX",
