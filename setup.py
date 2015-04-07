fc269af5fa6224052e4f77719e789d2587f7dc6f
Erik de Jonge <erik@a8.nl>
2015-04-07 14:15:14 +0200

    pip Tuesday 07 April 2015 (week:14 day:97), 14:15:12

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='arguments',
-      version='35',
+      version='36',
       description='Argument parser based on docopt',
       url='https://github.com/erikdejonge/arguments',
       author='Erik de Jonge',
