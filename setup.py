e3968113f0b45eaec6823cd895407a9b4e49ee4a
Erik de Jonge <erik@a8.nl>
2015-06-04 12:09:09 +0200

    Joseph Jackson: Its all about the money. Thursday 04 June 2015 (week:22 day:155), 12:09:09

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='arguments',
-      version='47',
+      version='48',
       description='Argument parser based on docopt',
       url='https://github.com/erikdejonge/arguments',
       author='Erik de Jonge',
