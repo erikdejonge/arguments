449bdce1c7674fe166924e9d2085b292959c1eec
Erik de Jonge <erik@a8.nl>
2015-06-19 16:16:45 +0200

    Albert Pujols: We are made to know and love God. Friday 19 June 2015 (week:24 day:170), 16:16:44

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='arguments',
-      version='54',
+      version='55',
       description='Argument parser based on docopt',
       url='https://github.com/erikdejonge/arguments',
       author='Erik de Jonge',
