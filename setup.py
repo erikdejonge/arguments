236b8acb1bb880b619c30daba90e185860def601
Erik de Jonge <erik@a8.nl>
2015-07-08 14:22:58 +0200

    Richard M. Nixon: Women are the only exploited group in history to have been idealized into powerlessness. Wednesday 08 July 2015 (week:27 day:189), 14:22:55

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='arguments',
-      version='61',
+      version='63',
       description='Argument parser based on docopt',
       url='https://github.com/erikdejonge/arguments',
       author='Erik de Jonge',
